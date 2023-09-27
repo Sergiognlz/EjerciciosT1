83423b2e4f4d5dfa697c5534a26dff2d5be847fd
Sgonzalez <Sgonzalez@106-14.info2>
2023-09-27 10:23:31 +0200

    2

--- src/ejerciciosTema1/Ejercicio2.java
+++ src/ejerciciosTema1/Ejercicio2.java
@@ -6,7 +6,7 @@ public class Ejercicio2 {
 public static void main(String[] args) {
 	//Guardamos la variable del número que vamos a usar
 	int  numero1;
-	int numero2;
+	int resto;
 	//Creamos el escaner
 	Scanner sc=new Scanner(System.in);
 	
@@ -14,7 +14,8 @@ public static void main(String[] args) {
 	System.out.println("Introduce un número");
 	//Leemos el número introducido
 	numero1 = sc.nextInt();
-	System.out.println("Hay que sumar "+ (numero1%7) + " a " + numero1 + " para que sea múltiplo de 7");
+	resto= (numero1%7);
+	System.out.println("Hay que sumar "+ (resto==0 ? 0:7-resto )+ " a " + numero1 + " para que sea múltiplo de 7");
 	
 	//cerramos escaner
 	sc.close();
